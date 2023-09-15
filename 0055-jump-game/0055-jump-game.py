ad2b4cf70d7a532c24f6fc588ed3648ff587f672
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-09-16 00:40:34 +0530

    Time: 413 ms (63.39%), Space: 17.5 MB (60.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,3 +1,20 @@
+# from typing import List
+
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         max_reach = 0
+        
+#         for i in range(n):
+#             # If index is beyond the maximum reach, return False
+#             if i > max_reach:
+#                 return False
+            
+#             # Update the reach
+#             max_reach = max(max_reach, i + nums[i])
+        
+#         # If the reach is greater than or equal to the last index, return True
+#         return max_reach >= n - 1
 from typing import List
 
 class Solution:
@@ -12,7 +29,11 @@ class Solution:
             
             # Update the reach
             max_reach = max(max_reach, i + nums[i])
+            
+            # If the maximum reach covers the last index, return True
+            if max_reach >= n - 1:
+                return True
         
-        # If the reach is greater than or equal to the last index, return True
-        return max_reach >= n - 1
-        
+        return True  # If the loop completes, it means you can reach the end
+
+
