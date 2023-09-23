3c1a4b882aa5bb8330680e935a8d2c0b5033e4f5
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-09-23 23:57:45 +0530

    Time: 379 ms (98.47%), Space: 38.3 MB (42.66%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,3 +1,5 @@
+# 1st approach
+'''
 from sortedcontainers import SortedList
 import statistics
 
@@ -11,3 +13,26 @@ class MedianFinder:
 
     def findMedian(self) -> float:
         return statistics.median(self.list)
+'''
+# 2nd approach
+import heapq
+
+class MedianFinder:
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heapq.heappush(self.large, -heapq.heappushpop(self.small, -num))
+        else:
+            heapq.heappush(self.small, -heapq.heappushpop(self.large, num))
+
+    def findMedian(self) -> float:
+        if len(self.small) == len(self.large):
+            return (self.large[0] - self.small[0]) / 2.0
+        else:
+            return float(self.large[0])
