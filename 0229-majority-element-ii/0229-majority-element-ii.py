49e23565ecfcb118c85be9b88c22edbc88a309c7
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-10-05 22:22:35 +0530

    Time: 105 ms (84.77%), Space: 17.8 MB (15.15%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,13 +1,31 @@
+#                                                     Approach One 
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> List[int]:
+#         freq={}
+#         ans=[]
+#         for i in range(0,len(nums)):
+#             if(nums[i] in freq):
+#                 freq[nums[i]]+=1
+#             else:
+#                 freq[nums[i]]=0
+#         for i in freq:
+#             if freq[i]>=len(nums)//3:
+#                 ans.append(i)
+#         return ans
+
+
+
+#                                                     Approach Two
+
+
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        freq={}
-        ans=[]
-        for i in range(0,len(nums)):
-            if(nums[i] in freq):
-                freq[nums[i]]+=1
-            else:
-                freq[nums[i]]=0
-        for i in freq:
-            if freq[i]>=len(nums)//3:
-                ans.append(i)
-        return ans
+        num = list(set(nums))
+        ls = []
+        for i in num:
+            d = nums.count(i)
+            if d > len(nums) // 3:
+                ls.append(i)
+        return ls
