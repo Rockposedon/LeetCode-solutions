ade99aa3f4505083af333c1a27d0ed3afe96c3de
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-10-06 09:57:42 +0530

    Time: 143 ms (88.43%), Space: 17.9 MB (52.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,20 +1,25 @@
 # <------------------------------Using Boyer Moore Majority vote algorithm------------------------------------------------>
 
 
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
         
-        count=0
-        result=0
+#         count=0
+#         result=0
         
-        for n in nums:
-            if count==0:
-                result=n
-            if n==result:
-                count+=1
-            else:
-                count-=1
-        return result
+#         for n in nums:
+#             if count==0:
+#                 result=n
+#             if n==result:
+#                 count+=1
+#             else:
+#                 count-=1
+#         return result
 
                 
-        
+# <-----------------------------another approach using sort and len ----------------------------------------------------->
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        n = len(nums)
+        return nums[n//2]
