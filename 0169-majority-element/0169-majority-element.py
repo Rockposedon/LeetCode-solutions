3f33625983360fc808573b7c376f16533108fd41
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-10-06 19:10:30 +0530

    Time: 155 ms (37.29%), Space: 17.7 MB (85.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -18,8 +18,28 @@
 
                 
 # <-----------------------------another approach using sort and len ----------------------------------------------------->
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         nums = sorted(nums)
+#         n = len(nums)
+#         return nums[n//2]
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         dic = {}
+#         for i in nums :
+#             if i in dic:
+#                 dic[i] += 1
+#             else :
+#                 dic[i] -= 1
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        n = len(nums)
-        return nums[n//2]
+        ele_dic = {}
+    
+        for i in nums:
+        
+            ele_dic[i] = ele_dic.get(i, 0) + 1
+    
+        for num, count in ele_dic.items():
+            if count > len(nums) // 2:
+                return num
