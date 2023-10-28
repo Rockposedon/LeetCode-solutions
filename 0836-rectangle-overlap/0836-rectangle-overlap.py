32f9255a9516dbb99d3e507c6ae0d868adb8a2f6
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-10-28 22:02:22 +0530

    Time: 43 ms (20.90%), Space: 16.4 MB (10.87%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.py
+++ 0836-rectangle-overlap/0836-rectangle-overlap.py
@@ -1,9 +1,26 @@
+# Approach 1st
+
+# class Solution:
+#     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+
+#         [A,B,C,D], [E,F,G,H] = rec1, rec2
+#         if (F>=D or E>=C or B>=H or A>=G):
+#             return False
+#         elif F == H or E == G or B == D or A == C:
+#             return False
+#         return True        
+
+
+# Approach 2nd
 class Solution:
     def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        """
+        the idea here is to check the cases where no overlap. 
+        """
+        x1, y1, x2, y2 = rec1
+        x11, y11, x22, y22 = rec2
 
-        [A,B,C,D], [E,F,G,H] = rec1, rec2
-        if (F>=D or E>=C or B>=H or A>=G):
-            return False
-        elif F == H or E == G or B == D or A == C:
-            return False
-        return True        
+        if y11 >= y2 or y22 <= y1 or x11 >= x2 or x22 <= x1:
+            return False # no overlap
+
+        return True     # there is an overlap
