1cf14cbad43f609ea1b73e025a0b68695becb2c4
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-11-04 06:43:22 +0530

    Time: 139 ms (93.53%), Space: 17.4 MB (30.22%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.py
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.py
@@ -1,49 +1,28 @@
-# class Solution:
-#     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-#         if left is None and right is None :
-#             return 0
-
-
-#         # If there are ants on the left side only, the last moment is the maximum position of ants on the left side.
-#         elif left and not right:
-#             return max(left)
-
-#         # If there are ants on the right side only, the last moment is the maximum position of ants on the right side.
-#         elif right and not left:
-#             return n - min(right)
-
-#         l = -1
-#         r = -1
-
-#         # Find the maximum position of ants on the left side.
-#         for left_index in left:
-#             l = max(l, left_index)
-
-#         # Find the maximum position of ants on the right side. Note that we subtract the minimum right position from n.
-#         for right_index in right:
-#             r = max(r, right_index)
-
-#         # The last moment is determined by the maximum position between left and right sides.
-#         return max(l,r)
-
 class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
-        # no ants, no time! 
-        if left is None and right is None : 
-            return 0 
-        elif left and not right : 
-            # only got ants heading left, we need the maximal position of that lefty 
+        if left is None and right is None :
+            return 0
+
+
+        # If there are ants on the left side only, the last moment is the maximum position of ants on the left side.
+        elif left and not right:
             return max(left)
-        elif right and not left : 
+
+        # If there are ants on the right side only, the last moment is the maximum position of ants on the right side.
+        elif right and not left:
             return n - min(right)
 
+        l = -1
+        r = -1
 
-        left_max = -1
-        right_max = -1 
-        for left_index in left :
-            left_max = max(left_max, left_index)
-        for right_index in right : 
+        # Find the maximum position of ants on the left side.
+        for left_index in left:
+            l = max(l, left_index)
 
-            right_max = max(right_max, n-right_index)
+        # Find the maximum position of ants on the right side. Note that we subtract the minimum right position from n.
+        for right_index in right:
+            r = max(r, n-right_index)
 
-        return max(left_max, right_max)
+        # The last moment is determined by the maximum position between left and right sides.
+        return max(l,r)
+
