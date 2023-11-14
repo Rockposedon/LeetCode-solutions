b0f105ecd9db8fc162c6531d2d983c83f6db5089
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-11-14 21:37:55 +0530

    Time: 2532 ms (26.77%), Space: 22 MB (5.26%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        # Initialize a dictionary to store the indices of each character in the string.
+        letter_count = {}
+
+        # Populate the letter_count dictionary with the indices of each character.
+        for i in range(len(s)):
+            if s[i] in letter_count:
+                letter_count[s[i]].append(i)
+            else:
+                letter_count[s[i]] = [i]
+
+        # Initialize a set to store unique palindromes of length three.
+        palindromes_set = set()
+
+        # Iterate through the letter_count dictionary.
+        for key in letter_count:
+            # Check if there are at least two occurrences of the character.
+            if len(letter_count[key]) >= 2:
+                # Iterate through the indices of the character to form palindromes.
+                for count in range(letter_count[key][0] + 1, letter_count[key][-1]):
+                    # Check if the index is within the bounds of the string.
+                    if count < len(s) - 1:
+                        # Add the palindrome to the set.
+                        palindromes_set.add(f"{key}{s[count]}{key}")
+
+        # Return the count of unique palindromic subsequences of length three.
+        return len(palindromes_set)
