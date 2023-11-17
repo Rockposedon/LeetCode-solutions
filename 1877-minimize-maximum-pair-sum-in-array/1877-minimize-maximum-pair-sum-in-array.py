8c0da094d470c3b573a5084a7327c15ac8984735
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2023-11-17 23:18:09 +0530

    Time: 944 ms (98.22%), Space: 30.7 MB (52.12%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -1,29 +1,53 @@
+# class Solution:
+#     def minPairSum(self, nums: List[int]) -> int:
+#         # Sort the array in ascending order
+#         nums.sort()
+        
+#         # Initialize pointers for the start and end of the sorted array
+#         start = 0
+#         end = len(nums) - 1
+        
+#         # Initialize variable to track the maximum pair sum
+#         max_sum = 0
+        
+#         # Pair elements from the two ends of the sorted array
+#         while start < end:
+#             # Calculate the pair sum for the current pair
+#             result = nums[start] + nums[end]
+            
+#             # Move the pointers inward
+#             start += 1
+#             end -= 1
+            
+#             # Update the maximum pair sum if the current pair sum is greater
+#             if result > max_sum:
+#                 max_sum = result
+        
+#         # Return the minimized maximum pair sum
+#         return max_sum
+
+
+
+""" Another 
+    approach dividing list into two halves and sort another list in reverse order then find sum"""
+    
+    
 class Solution:
     def minPairSum(self, nums: List[int]) -> int:
-        # Sort the array in ascending order
+        
         nums.sort()
+        mid = len(nums) // 2
+        first_list = nums[:mid]
+        second_list = nums[mid:]
         
-        # Initialize pointers for the start and end of the sorted array
-        start = 0
-        end = len(nums) - 1
-        
-        # Initialize variable to track the maximum pair sum
-        max_sum = 0
-        
-        # Pair elements from the two ends of the sorted array
-        while start < end:
-            # Calculate the pair sum for the current pair
-            result = nums[start] + nums[end]
-            
-            # Move the pointers inward
-            start += 1
-            end -= 1
-            
-            # Update the maximum pair sum if the current pair sum is greater
-            if result > max_sum:
-                max_sum = result
-        
-        # Return the minimized maximum pair sum
-        return max_sum
-    
-    
+
+        # Sort the second list in descending order
+        second_list_sorted_desc = sorted(second_list, reverse=True)
+
+        # Pair up the elements
+        pairs = zip(first_list, second_list_sorted_desc)
+
+        # Find the maximum pair sum
+        max_pair_sum = max(pair[0] + pair[1] for pair in pairs)
+
+        return max_pair_sum
