4c518647e9af1ae8b505a2400e2d8f4f8a03d3ad
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-02-20 07:21:49 +0530

    Time: 108 ms (79.97%), Space: 17.8 MB (88.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,16 +1,16 @@
+# class Solution:
+
+#         """nums_set = set(nums) 
+#         l = max(nums)
+#         for i in range(0,l+2):
+#             if i not in nums_set:
+#                 return i 
+            
+#         return -1
+#         """
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-# '''        nums.sort()
-#         l = nums[-1]
-#         for i in range(0,l+2):
-#             if i not in nums :
-#                 return i
-#         return -1
-# '''   
-        nums_set = set(nums) 
-        l = max(nums)
-        for i in range(0,l+2):
-            if i not in nums_set:
-                return i 
-            
-        return -1
+        n = len(nums)
+        expected_sum = (n * (n + 1)) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
