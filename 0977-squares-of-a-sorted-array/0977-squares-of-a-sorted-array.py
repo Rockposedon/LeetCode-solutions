edad909318ac7ef836d0fc749846a9dfc16dcac6
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-03-03 11:07:36 +0530

    Time: 153 ms (72.69%), Space: 18.8 MB (71.64%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(0,n):
             a = nums[i]**2
             new_square.append(a)
-            new_square.sort()
+        new_square.sort()
             
         return new_square
             
