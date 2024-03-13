c08b6eefd9aa0ac7b40f7a9d8300c8fab76533d9
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-03-13 14:46:44 +0530

    Time: 742 ms (14.67%) | Memory: 16.4 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,11 +1,19 @@
+# class Solution:
+#     def pivotInteger(self, n: int) -> int:
+#         if n == 1:
+#             return 1
+#         if n == 8:
+#             return 6
+#         if n == 49:
+#             return 35
+#         if n == 288:
+#             return 204
+#         return -1 
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 8:
-            return 6
-        if n == 49:
-            return 35
-        if n == 288:
-            return 204
-        return -1 
+        for i in range(1,n+1):
+            sum_left = sum(range(1,i+1))
+            sum_right = sum(range(i,n+1))
+            if sum_left == sum_right:
+                return i
+        return -1
