af0f665cec15590fdb033b61fae28ca12e66e6e9
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-03-26 23:08:35 +0530

    Time: 265 ms (95.85%), Space: 30.2 MB (94.68%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,10 +1,14 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        set1= set(nums)
-        i = 1
-
-        # checking membership of each element
-        while i in set1:
-            i += 1
-        return i
+        n = len(nums)
+        seen = [False]*(n+1)
         
+        for i in nums:
+            if 0 < i <= n :
+                seen[i] = True
+        
+        for j in range(1,n+1):
+            if not seen[j]:
+                return j
+        
+        return n+1
