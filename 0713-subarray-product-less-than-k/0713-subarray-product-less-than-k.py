e0f33c8eb8f9630941ee1f11f302655a4391456c
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-03-27 11:14:02 +0530

    Time: 496 ms (78.37%), Space: 19.3 MB (16.13%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -1,3 +1,4 @@
+"""
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         n = len(nums)
@@ -15,3 +16,88 @@ class Solution:
             count += right - left + 1
         
         return count
+"""
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        left = 0
+        right = 0
+        product = 1
+        count = 0
+        n = len(nums)
+        while right < n :
+            product *= nums[right]
+            while product >= k and left <= right:
+                product //= nums[left]
+                left += 1
+            count += (right-left) + 1
+            right += 1
+        return count 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
