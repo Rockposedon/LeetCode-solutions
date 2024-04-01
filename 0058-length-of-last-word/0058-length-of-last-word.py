e613a9989790c4cbf99233f510994f724d1e8622
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-04-01 23:38:53 +0530

    Time: 34 ms (61.79%), Space: 16.5 MB (98.80%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,18 +1,4 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # strip is used to remove spaces from begning and end 
-        s = s.strip()
-        
-        # split is used to split string with space 
-        s = s.split()
-        
-        #  slicing to use last word of string
-        s = s[-1]
-        
-        # return the length of last word
-        return len(s)
-        
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        x = s.split()
+        return len(x[-1])
