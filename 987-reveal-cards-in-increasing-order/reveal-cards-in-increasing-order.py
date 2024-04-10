91da21bf5af300fe14193db6c23b3221ca01f688
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-04-11 00:24:46 +0530

    Time: 56 ms (6.18%) | Memory: 16.7 MB (80.95%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -0,0 +1,7 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        ans=[]
+        deck.sort(reverse=True)
+        for i in deck:
+            ans=[i]+ans[-1:]+ans[:-1]
+        return ans  
