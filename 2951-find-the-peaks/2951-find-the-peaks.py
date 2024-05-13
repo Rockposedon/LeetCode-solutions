6df83b7dfcf5855f0d3e83d3a91c4a5f6f8afd76
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-05-14 00:22:17 +0530

    Time: 53 ms (26.60%), Space: 16.4 MB (89.44%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.py
+++ 2951-find-the-peaks/2951-find-the-peaks.py
@@ -1,7 +1,16 @@
 class Solution:
     def findPeaks(self, mountain: List[int]) -> List[int]:
+        # peak list to store the peak of mountain
         peak = []
+        
+        # Iterating in mountain while excluding first and last element
         for i in range(1,len(mountain)-1):
+            
+            # Checking peak element conditions 
             if mountain[i-1] < mountain[i] > mountain[i+1]:
+                
+                # Adding index of peak from mountain in peak list
                 peak.append(i)
+                
+        # Returning index list of peak from mountain
         return peak
