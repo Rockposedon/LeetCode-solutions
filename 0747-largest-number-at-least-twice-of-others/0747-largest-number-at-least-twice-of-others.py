55f754dde6d26236f45f3cac927cf37d27c4efec
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-06-10 15:24:08 +0530

    Time: 37 ms (68.47%), Space: 16.5 MB (34.11%) - LeetHub

---
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
@@ -0,0 +1,21 @@
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        
+        # Find the largest number in the list
+        largest = max(nums)
+        
+        # Find the index of the largest number
+        maxIndex = nums.index(largest)
+        
+        # Check if the largest number is more than twice any other number
+        for i in nums:
+            
+            # Skip the largest number itself
+            if i!= largest:
+                
+                # If any other number is more than half of the largest number, return -1
+                if i * 2 > largest:
+                    return -1
+                
+        # If no such number is found, return the index of the largest number
+        return maxIndex
