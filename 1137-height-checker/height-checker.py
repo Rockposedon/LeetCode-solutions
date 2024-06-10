cfc1441cb27caecdd484adf821cea3d650767ecb
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-06-10 13:41:42 +0530

    Time: 42 ms (37.58%) | Memory: 16.5 MB (29.57%) - LeetSync

--- 1137-height-checker/height-checker.py
+++ 1137-height-checker/height-checker.py
@@ -1,11 +1,24 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
+        
+        # Create a sorted copy of the input list, hs->(sorted heights)
         hs = sorted(heights)
+        
+        # If the input list is already sorted
         if heights == hs:
             return 0
+        
+        # Initialize a counter to keep track of the number of students that need to be moved
         count = 0
+        
+        # Iterate through the input list and the sorted list simultaneously
         for i in range(len(heights)):
-            if heights[i] != hs[i]:
+            
+            # If the current student in the input list is not in the correct position
+            if heights[i]!= hs[i]:
+                
+                # Increment the counter
                 count += 1
-            pass
+        
+        # Return the total number of students that need to be moved
         return count
