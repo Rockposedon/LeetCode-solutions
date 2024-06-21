ac811f85e332b19e62c609fadd3a1a27332c5eb9
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-06-21 14:49:06 +0530

    Time: 1158 ms (16.08%), Space: 19.2 MB (96.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,21 +1,38 @@
-from heapq import heappush, heappop
-from typing import List, Optional
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        test = ListNode(0)
+        tail = test
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        return test.next
+
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        heap, res = [], ListNode()
+        if not lists:
+            return None
+
+        temp = [lists[0]]
+        for i in range(len(lists) - 1):
+            x = self.mergeTwoLists(temp[i], lists[i + 1])
+            temp.append(x)
+        return temp[-1]
     
-        # Initialize the min-heap with the first node from each list
-        for i, lst in enumerate(lists):
-            if lst:
-                heappush(heap, (lst.val, i, lst))
-
-        cur = res  # 'cur' is a pointer used to build the merged linked list
-        while heap:
-            _, i, lst = heappop(heap)  # Get the smallest element from the heap
-            if lst.next:
-                heappush(heap, (lst.next.val, i, lst.next))  # Push the next element of the list to the heap if it exists
-
-            cur.next, cur = lst, lst  # Add the current node to the merged list
-
-        return res.next  # 'res.next' is the merged linked list starting from the first node
+    
+    
