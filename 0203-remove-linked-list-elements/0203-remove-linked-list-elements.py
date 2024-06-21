bdc757876c26271c7a4978ec2d5478ef9cc1fe1a
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-06-21 17:33:06 +0530

    Time: 49 ms (56.36%), Space: 18.5 MB (98.03%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.py
@@ -3,37 +3,44 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-#         node = head
-#         while node and node.next:
-#             if node.next.val == val:
-#                 node.next = node.next.next
-#             else:
-#                 node = node.next
-        
-#         if head.val == val:
-#             head = head.next
-#         return head
 class Solution:
     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
         
-        # Create a dummy node to simplify edge cases where head needs to be removed
-        dummy = ListNode(0)
-        dummy.next = head
+        if not head:
+            return head
         
-        # Initialize the current pointer to the dummy node
-        current = dummy
-        
-        # Traverse the list
-        while current.next:
-            # If the next node has the value to be removed, bypass it
-            if current.next.val == val:
-                current.next = current.next.next
+        node = head
+        while node and node.next:
+            if node.next.val == val:
+                node.next = node.next.next
             else:
-                # Otherwise, move to the next node
-                current = current.next
+                node = node.next
         
-        # Return the new head, which is the next node of dummy
-        return dummy.next
+        if head.val == val:
+            head = head.next
+        return head
+    
+
+
+# class Solution:
+#     def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+#         # Create a dummy node to simplify edge cases where head needs to be removed
+#         dummy = ListNode(0)
+#         dummy.next = head
+        
+#         # Initialize the current pointer to the dummy node
+#         current = dummy
+        
+#         # Traverse the list
+#         while current.next:
+#             # If the next node has the value to be removed, bypass it
+#             if current.next.val == val:
+#                 current.next = current.next.next
+#             else:
+#                 # Otherwise, move to the next node
+#                 current = current.next
+        
+#         # Return the new head, which is the next node of dummy
+#         return dummy.next
 
