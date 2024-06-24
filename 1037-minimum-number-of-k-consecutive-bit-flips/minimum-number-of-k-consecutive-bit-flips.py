7bc4e02b7da61a6cd394bf1f357b6effbb279c37
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-06-25 00:10:19 +0530

    Time: 779 ms (90.84%) | Memory: 19.7 MB (35.21%) - LeetSync

---
+++ 1037-minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minKBitFlips(self, nums: List[int], k: int) -> int:
+        current_flips = 0  # Tracks the current number of flips
+        total_flips = 0  # Tracks the total number of flips
+
+        for i in range(len(nums)):
+            # If the window slides out of the range and the leftmost element is
+            #  marked as flipped (2), decrement current_flips
+            if i >= k and nums[i - k] == 2:
+                current_flips -= 1
+
+            # Check if the current bit needs to be flipped
+            if (current_flips % 2) == nums[i]:
+                # If flipping would exceed array bounds, return -1
+                if i + k > len(nums):
+                    return -1
+                # Mark the current bit as flipped
+                nums[i] = 2
+                current_flips += 1
+                total_flips += 1
+
+        return total_flips
