8a32f4131b3d53248ade2bcacbad447f397ded56
Paritosh Verma <101548529+Rockposedon@users.noreply.github.com>
2024-08-03 23:35:04 +0530

    Time: 75 ms (51.41%) | Memory: 16.7 MB (88.16%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
         for i in target:
             if i not in arr:
@@ -8,3 +8,13 @@ class Solution:
                 arr.sort()
                 if target == arr:
                     return True
+"""
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        target.sort()
+        arr.sort()
+        for i in range(len(arr)):
+            if  arr[i] != target[i]:
+                return False
+        return True
